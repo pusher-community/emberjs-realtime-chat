906adc16346e37adf447a285b41bb13c87bddd54
Jack Franklin <jack@jackfranklin.net>
2015-10-28 13:37:26 +0000

    Add the Ember logo

--- app/components/chat-message.js
+++ app/components/chat-message.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   timestamp: Ember.computed('message', function() {
-    return strftime('%H:%M:%S %P', new Date(this.get('message').time))
+    return strftime('%H:%M:%S %P', new Date(this.get('message').time));
   })
 });
